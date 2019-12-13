9e5145d412a1a459f9844bd326927fbf6be28742
Felipe <felipehfsouza@gmail.com>
2019-12-13 22:01:07 +0000

    try/catch e status code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,43 +4,52 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     async store(req, res){
-        const { email, password } = req.body;
-        let user = await User.findOne({ email });
-        
-        if (!user){
-            const hashedPassword = await bcrypt.hash(password, 10);
+        try {
+            const { email, password } = req.body;
+            let user = await User.findOne({ email });
             
-            user = await User.create({ email, password: hashedPassword });
+            if (!user){
+                const hashedPassword = await bcrypt.hash(password, 10);
+                
+                user = await User.create({ email, password: hashedPassword });
+            }
+    
+            user.password = undefined;
+            return res.status(201).json(user);
+        } catch(err) {
+            return res.status(500).json({ errors: [err]});
         }
-
-        user.password = undefined;
-        return res.json(user);
     },
     async login(req, res) {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(404).json({ errors: [ 'O e-mail não foi encontrado!']});
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({ email });
+    
+            if (!user) {
+                return res.status(404).json({ errors: [ 'O e-mail não foi encontrado!']});
+            }
+    
+            const isPasswordCorrect = await bcrypt.compare(password, user.password);
+            if (!isPasswordCorrect) {
+                return res.status(400).json({ errors: ['Senha inválida.']});
+            }
+    
+            const payload = {
+                id: user.id, 
+                email: user.email
+            };
+    
+            const token = jwt.sign(payload, process.env.SECRET || "s3cr3t", {
+                expiresIn: "7d"
+            });
+    
+            return res.json({
+                token,
+                ...payload
+            });
+        } catch(err) {
+            return res.status(500).json({ errors: [err]});
         }
-
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) {
-            return res.status(400).json({ errors: ['Senha inválida.']})
-        }
-
-        const payload = {
-            id: user.id, 
-            email: user.email
-        };
-
-        const token = jwt.sign(payload, process.env.SECRET || "s3cr3t", {
-            expiresIn: "7d"
-        });
-
-        return res.json({
-            token,
-            ...payload
-        });
     }
 };
